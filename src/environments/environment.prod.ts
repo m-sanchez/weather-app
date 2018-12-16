1e420a4210e30155dbc442554481ea31e6743f9a
Miguel Sánchez <contact@miguelsanchez.co.uk>
2018-12-16 22:28:25 +0000

    https issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
+  apiHost: '//api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
+  apiHost: '//api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
 };
 
 /*
