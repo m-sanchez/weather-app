43c36cb5289a430b046c9ac9793a7d3560ac762f
Miguel Sánchez <contact@miguelsanchez.co.uk>
2018-12-16 22:18:01 +0000

    changes for heroku deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=53e1473575ea89231d25ea42cd43319e'
+  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=53e1473575ea89231d25ea42cd43319e'
+  apiHost: 'http://api.openweathermap.org/data/2.5/forecast?APPID=eb25fadc5f85d4ab02e38e7e09be323a'
 };
 
 /*
